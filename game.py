f2a9c4a89752ccd61f7c8e7adcffe20262dd2157
BaoQuangNguyen <bnguyen0239@gmail.com>
2023-07-06 10:30:50 -0500

    Landscaper App: user can buy and use scissors to cut grass

--- game.py
+++ game.py
@@ -4,16 +4,36 @@ tools = {
     "teeth": 1,
 }
 
-while True:
-    cut = input("What tool would you like to cut grass with today? (teeth) ")
+buy_tools = {
+    "rusty scissors": 5,
+}
 
-    if cut == "quit":
+while True:
+    print(f"You currently have ${money}. Your available tools: {', '.join(tools.keys())}")
+    
+    cut = input("What tool would you like to cut grass with today? ")
+
+    if cut.lower() == "quit":
         break
 
     if cut in tools:
         money += tools[cut]
-        print(f"You cut grass with {cut} and earned ${tools[cut]}! You currently have ${money} ")
+        print(f"You cut grass with {cut} and earned ${tools[cut]}! ")
+
+        if money >= 5 and "rusty scissors" not in tools.keys():
+            print("rusty scissors is available for purchase! ")
+
+    elif cut == "buy":
+        buy = input("What tool would you like to buy? (rusty scissors) ")
+        if buy in buy_tools:
+            price = buy_tools[buy]
+            if money >= price:
+                money -= price
+                tools[buy] = buy_tools[buy]
+                print(f"You bought {buy} for ${price}! ")
+            else:
+                print("Not enough money!")
+        else:
+            print("Not a valid tool!")
     else:
         print("Not a tool!")
-
-
